b0e4e1564e3aa7756d6c00071508f6c609ff7813
SVPLKU <SVPLKU@N5192.slake.sssb.com>
2020-12-14 23:13:45 +0800

    initial commit

---
+++ src/main/java/org/getstarted/spring/customerrestservice/CustomerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.getstarted.spring.customerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/getstarted/spring/customerrestservice/CustomerRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.getstarted.spring.customerrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
